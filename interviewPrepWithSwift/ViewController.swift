c77d6d03ce199c62d35f543140d117d03f0c4684
@tPlisk <ryanpliske@yahoo.com>
2015-07-18 10:40:59 -0500

    Initial Commit

---
+++ interviewPrepWithSwift/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  interviewPrepWithSwift
+//
+//  Created by Ryan Pliske on 7/17/15.
+//  Copyright (c) 2015 Ryan Pliske. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var ryansName : NSString = NSString()
+        if (ryansName.length == 4) {
+            print("Ryan's name is four letters long")
+        }
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
